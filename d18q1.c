d80ad5a5d7d14d61c4c3e8660d4bc16e7ad16b3e
karunachawla000 <karunachawla000@gmail.com>
2026-02-20 18:09:43 +0530

    Create d18q1.c

---
+++ d18q1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n], temp[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int k;
+    scanf("%d", &k);
+
+    k = k % n;
+
+    for(int i = 0; i < n; i++) {
+        temp[(i + k) % n] = arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", temp[i]);
+    }
+
+    return 0;
+}
