f5b6e37ac809b55e280ec3b3d258a491dec4e017
karunachawla000 <karunachawla000@gmail.com>
2026-03-02 13:45:25 +0530

    Create d30q1.c

---
+++ d30q1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+// Insert at end
+struct Node* insertTerm(struct Node* head, int c, int e) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = c;
+    newNode->exp = e;
+    newNode->next = NULL;
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Print polynomial
+void printPolynomial(struct Node* head) {
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+
+        if (temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if (temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if (temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, c, e;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &c, &e);
+        head = insertTerm(head, c, e);
+    }
+
+    printPolynomial(head);
+
+    return 0;
+}
