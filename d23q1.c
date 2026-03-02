15a23c26cda5cb27735eae3313f6118e15d5e231
karunachawla000 <karunachawla000@gmail.com>
2026-03-02 13:15:33 +0530

    Create d23q1.c

---
+++ d23q1.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+    int value;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+// Function to merge two sorted lists
+struct Node* mergeLists(struct Node* head1, struct Node* head2) {
+    struct Node dummy;   // temporary dummy node
+    struct Node* tail = &dummy;
+    dummy.next = NULL;
+
+    while(head1 != NULL && head2 != NULL) {
+        if(head1->data <= head2->data) {
+            tail->next = head1;
+            head1 = head1->next;
+        } else {
+            tail->next = head2;
+            head2 = head2->next;
+        }
+        tail = tail->next;
+    }
+
+    // Attach remaining nodes
+    if(head1 != NULL)
+        tail->next = head1;
+    else
+        tail->next = head2;
+
+    return dummy.next;
+}
+
+// Function to print list
+void printList(struct Node* head) {
+    while(head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, m;
+
+    scanf("%d", &n);
+    struct Node* head1 = createList(n);
+
+    scanf("%d", &m);
+    struct Node* head2 = createList(m);
+
+    struct Node* mergedHead = mergeLists(head1, head2);
+
+    printList(mergedHead);
+
+    return 0;
+}
